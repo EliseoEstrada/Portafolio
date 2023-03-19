d63f0cbc9c07bf23b8f3e11e8dd1b6bcf26a9076
Eliseo Guadalupe Estrada Covarrubias <eliseo1677@outlook.com>
2023-03-19 05:44:27 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.card').hover(
+    function(){ $(this).addClass('animate__headShake') },
+    function(){ $(this).removeClass('animate__headShake') }
+)
+
+$('.tag').hover(
+    function(){ $(this).addClass('animate__headShake') },
+    function(){ $(this).removeClass('animate__headShake') }
+)
+
+
